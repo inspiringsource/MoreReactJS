8917e98b379c8ef8d660f84c9d57c2c57bd326a9
Abe <ab.b2k3@gmail.com>
2021-09-25 11:50:25 +0200

    Passing Data to Components

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import logo from './logo.svg'
 import './App.css'
 import React, { Component } from 'react'
 
-function Body () {
-  return(
-  <div>
-    <p>
-      Edit <code>src/App.js</code> and save to rock.
+function Body(props) {
+  return (
+    <div>
+    <p className="App-intro">
+      {props.text}<br/> 
+      {props.text2}
     </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </div>
-  );
+    </div>
+  )
 }
 
 class Header extends Component {
@@ -25,16 +18,36 @@ class Header extends Component {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title"> {this.props.title}</h1>
+        <div>
+          Number =
+          {this.props.num}
+          <br/>Object b =
+          {this.props.myObj.b}
+          <br/>My Array [2]= 
+          {this.props.myArr[2]}
+          <br/>My Function a+b= 
+          {this.props.myFunc(10, 12)}
+        </div>
       </header>
     )
   }
 }
 
 function App() {
+
+
   return (
     <div className="App">
-      <Header />
-      <Body />
+      <Header 
+      title="You are cool" 
+      num={1} 
+      myObj={{ a: 2.5, b: 2 }}
+      myArr={[1,2,3]}
+      myFunc={(a, b)=>a+b}/>
+      <Body 
+      text="Hello"
+      text2="Good-bye"/>
     </div>
   )
 }
