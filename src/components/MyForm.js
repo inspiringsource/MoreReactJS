fadac7e2c6d45146a967b5fe4ed1cfa1a20138d0
Abe <ab.b2k3@gmail.com>
2021-09-27 13:52:07 +0200

    Form Validation

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,60 +1,65 @@
-import React from "react";
+import React from 'react'
 
 export default class MyForm extends React.Component {
   state = {
-    name: "Name",
-    favoritePet: "Pet name",
+    name: 'Name',
+    favoritePet: 'Pet name',
     rememberMe: false,
-    title: "Miss."
-  };
+    title: 'Miss.',
+  }
 
-  handleChange = event => {
-    const isCheckbox = event.target.type === "checkbox";
+  handleChange = (event) => {
+    const isCheckbox = event.target.type === 'checkbox'
     this.setState({
       [event.target.name]: isCheckbox
         ? event.target.checked
-        : event.target.value
-    });
-  };
+        : event.target.value,
+    })
+  }
 
-  handleSubmit = event => {
-    event.preventDefault();
-    console.log(this.state);
-  };
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.state)
+  }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input
-          name="name"
-          value={this.state.name}
-          onChange={this.handleChange}
-        />
-        <textarea
-          name="favoritePet"
-          value={this.state.favoritePet}
-          onChange={this.handleChange}
-        />
-        <input
-          name="rememberMe"
-          type="checkbox"
-          checked={this.state.rememberMe}
-          onChange={this.handleChange}
-        />
         <div>
+          <input size="20"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <br />
+          <textarea cols="26"
+            name="favoritePet"
+            value={this.state.favoritePet}
+            onChange={this.handleChange}
+          />
+        </div>
+        <br />
+        <div>
+          <input
+            name="rememberMe"
+            type="checkbox"
+            checked={this.state.rememberMe}
+            onChange={this.handleChange}
+          />{' '}
+          &nbsp;&nbsp;&nbsp;
           <select
             name="title"
             value={this.state.title}
             onChange={this.handleChange}
           >
-            <option>Mr.</option>
-            <option>Miss.</option>
-            <option>Ms.</option>
-            <option>Mrs.</option>
+            <option>Rock </option>
+            <option>paper </option>
+            <option>scissors </option>
           </select>
         </div>
+        <br />
         <button type="submit">submit</button>
       </form>
-    );
+    )
   }
 }
